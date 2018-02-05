99bdd728b7fc386388f2e73d73b823c5b71e65e7
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-02-05 05:49:29 -0300

    First commit

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <header>
+        <h1> {{ teste }} </h1>
+    </header>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      teste:'Vue Ninjas'
+    }
+  }
+}
+</script>
+<style scoped>
+header{
+  background: lightgreen;
+  padding:10px;
+}
+h1{
+  color:#222;
+  text-align: center;
+}
+</style>
